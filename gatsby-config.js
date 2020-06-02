77c3cbd23136997b70f252ae32d4e053bf9808fd
Jesús Bossa <x@jesusbossa.dev>
2020-06-01 20:15:56 -0500

    [WIP] Prepare md

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,9 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,8 +16,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `archive`,
+        path: `${__dirname}/src/archive`,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -24,11 +32,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,45 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  // console.log({ type: node.internal.type, internal: node.internal })
+  if (node.internal.type === `MarkdownRemark`) {
+    // console.log({ type: node.internal.type, internal: node.internal, rel: getNode(node.parent).relativePath })
+
+    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    console.log(slug.split('/'));
+    actions.createNodeField({
+      node,
+      name: `slug`,
+      value: `article/p=${slug.split('/')[1]}`,
+    });
+  }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+  // console.log(JSON.stringify(result, null, 4))
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    actions.createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/container/post/post.tsx`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug,
+      },
+    });
+  });
+};
