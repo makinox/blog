0cd3f586f6e5d64b5526c987c1d6eb0c1a18334d
Jesús Bossa <x@jesusbossa.dev>
2020-06-03 08:36:24 -0500

    [ADD] Container layout posts

--- src/container/post/post.tsx
+++ src/container/post/post.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Navbar, LayoutContainer } from '../../components';
 import { graphql } from 'gatsby';
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark;
   return (
-    <>
+    <LayoutContainer>
+      <Navbar />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
-    </>
+    </LayoutContainer>
   );
 }
 
