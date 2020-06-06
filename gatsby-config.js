39d30fe9e99129ff9aa681006fc1d1a5bfc97398
Jesús Bossa <x@jesusbossa.dev>
2020-06-05 22:03:36 -0500

    [ADD] Offline & service workers

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,8 +62,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://cereno.jesusbossa.dev',
-        sitemap: 'https://cereno.jesusbossa.dev/sitemap.xml',
+        host: 'https://blog.jesusbossa.dev',
+        sitemap: 'https://blog.jesusbossa.dev/sitemap.xml',
         policy: [{ userAgent: '*', allow: '/' }],
       },
     },
