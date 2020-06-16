d92ba1aad81de19afd522dd1098292c6dc99a900
Jesús Bossa <x@jesusbossa.dev>
2020-06-16 12:16:11 -0500

    [IMPROVEMENT] Modal content

--- src/components/modalContent/modalContent.tsx
+++ src/components/modalContent/modalContent.tsx
@@ -4,13 +4,13 @@ import { ModalContent, ModalTitle, ModalHeader, CloseIcon, ModalParragraph } fro
 export default ({ modalHandler }) => (
   <ModalContent>
     <ModalHeader>
-      <ModalTitle>Me elegra que te haya gustado</ModalTitle>
+      <ModalTitle>Me alegra que te haya gustado</ModalTitle>
       <CloseIcon onClick={modalHandler} />
     </ModalHeader>
     <div>
       <ModalParragraph>
-        Si te interesa colaborar, podrias ayudarme compartiendo el post o simplemente hablandome en{' '}
-        <a href="https://twitter.com/jesMakinox" target="_blank">
+        Si te interesa colaborar, podrías ayudarme compartiendo el post o simplemente hablandome en{' '}
+        <a href="https://twitter.com/jesMakinox" target="_blank" style={{ color: 'var(--linkColor)' }}>
           twitter
         </a>
       </ModalParragraph>
