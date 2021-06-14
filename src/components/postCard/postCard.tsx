adbd2ae806ebfa4fe4bba0806613d3dfbb9d8de3
Jesus Bossa <x@jesusbossa.dev>
2021-06-14 13:56:05 -0500

    chore: wip improve theme - post cards

--- src/components/postCard/postCard.tsx
+++ src/components/postCard/postCard.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { navigate } from 'gatsby';
 
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { CardContainer } from './postCard.styles';
 
+import { BlogContext } from '../../utils/context/context';
+
 export default ({ data }: { data: object | any }) => {
   const image = getImage(data.node.frontmatter.timage.postImage);
+  const { isDark } = useContext(BlogContext);
 
   return (
     <CardContainer
       use="elevated"
       maxWidth="auto"
-      onClick={() => navigate(`/${data.node.fields.slug}`)}
+      isDark={isDark}
       text={data.node.excerpt}
       title={data.node.frontmatter.title}
+      onClick={() => navigate(`/${data.node.fields.slug}`)}
       secondary={`${data.node.timeToRead} minutos de lectura`}
       customMedia={<GatsbyImage image={image} alt={`${data.node.frontmatter.title} imagen`} />}
     />
