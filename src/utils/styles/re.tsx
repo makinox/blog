3be07048096d748e230efbf87ee2b275f54f4c8d
Jesus Bossa <x@jesusbossa.dev>
2021-06-14 14:55:56 -0500

    chore: wip improve theme - post page

--- src/utils/styles/re.tsx
+++ src/utils/styles/re.tsx
@@ -1,37 +1,14 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { Link } from 'gatsby';
-import { FaFacebookSquare, FaTwitterSquare, FaLinkedin, FaHome } from 'react-icons/fa';
 
-const intent = css`
-  cursor: pointer;
-  margin: 0 5px;
-  font-size: 20px;
-  color: var(--textTercColor);
-`;
-
-export const FooterTags = styled(Link)`
+export const FooterTags = styled(Link)<{ isDark: boolean }>`
   display: inline-block;
   margin: 0 4px;
   padding: 2px;
   border-radius: 3px;
   text-decoration: none;
   color: #6a737d;
-  background-color: var(--backgroundSecondaryColor);
   box-shadow: 2px 2px 1px 0px rgba(0, 0, 0, 0.06);
-`;
 
-export const FacebookButton = styled(FaFacebookSquare)`
-  ${intent}
-`;
-
-export const LinkedinButton = styled(FaLinkedin)`
-  ${intent}
-`;
-
-export const TwitterButton = styled(FaTwitterSquare)`
-  ${intent}
-`;
-
-export const HomeButton = styled(FaHome)`
-  ${intent}
+  ${props => (props.isDark ? `color: rgb(var(--light-background));` : `color: rgb(var(--dark-background));`)}
 `;
