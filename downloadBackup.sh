295c64fff699f7eae2ce316cd5c95539393187d5
Jesus Bossa <x@jesusbossa.dev>
2021-07-14 21:12:01 -0500

    feat: 010 revision

--- downloadBackup.sh
+++ downloadBackup.sh
@@ -1,4 +1,4 @@
-curl https://download1338.mediafire.com/9wbh7yh17h0g/vmos4thf8fu7f2s/archiveImagesBackup.tar.gz -O
-curl https://download1641.mediafire.com/3f9d2lrzhr1g/vkoedzv6le2264f/archiveDocsBackup.tar.gz -O
+curl https://download1323.mediafire.com/j5gammyg3gfg/vmos4thf8fu7f2s/archiveImagesBackup.tar.gz -O
+curl https://download1498.mediafire.com/m324cpwfnltg/vkoedzv6le2264f/archiveDocsBackup.tar.gz -O
 tar -zxvf archiveImagesBackup.tar.gz
 tar -zxvf archiveDocsBackup.tar.gz
