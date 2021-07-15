d74afb916bbffa15a96e74f6e158a13001b7be8b
Jesus Bossa <x@jesusbossa.dev>
2021-07-14 20:51:27 -0500

    chore: automatize backups

---
+++ compress.sh
@@ -0,0 +1,2 @@
+tar -zcvf archiveImagesBackup.tar.gz ./src/images/archive
+tar -zcvf archiveDocsBackup.tar.gz ./src/archive
---
+++ downloadBackup.sh
@@ -0,0 +1,4 @@
+curl https://download1338.mediafire.com/9wbh7yh17h0g/vmos4thf8fu7f2s/archiveImagesBackup.tar.gz -O
+curl https://download1641.mediafire.com/3f9d2lrzhr1g/vkoedzv6le2264f/archiveDocsBackup.tar.gz -O
+tar -zxvf archiveImagesBackup.tar.gz
+tar -zxvf archiveDocsBackup.tar.gz
