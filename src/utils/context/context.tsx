26f541d6aa4293ee072cd921baa0f3c9ecdd7755
Jesus Bossa <x@jesusbossa.dev>
2021-07-23 18:34:38 -0500

    feat: improve dark mode

--- src/utils/context/context.tsx
+++ src/utils/context/context.tsx
@@ -1,12 +1,17 @@
 import React, { createContext, useState } from 'react';
+import { useEffect } from 'react';
 
 export const BlogContext = createContext({ isDark: false, changeTheme: () => {} });
 
 export const BlogProvider = ({ children }) => {
-  const [isDark, setIsDark] = useState(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+  const [isDark, setIsDark] = useState(false);
   const changeTheme = () => {
     setIsDark(prev => !prev);
   };
+
+  useEffect(() => {
+    setIsDark(Boolean(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches));
+  }, []);
 
   return (
     <BlogContext.Provider
