44c64d37f5a2ba6a792bf72bac42586ecbf35e31
Jesus Bossa <x@jesusbossa.dev>
2021-09-04 13:45:31 -0500

    chore: search input styled

--- src/components/searchContent/searchContent.styles.ts
+++ src/components/searchContent/searchContent.styles.ts
@@ -3,6 +3,38 @@ import styled from 'styled-components';
 export const SearchContainer = styled.div`
   top: 3%;
   margin-top: -30%;
-  border-radius: 4%;
+  border-radius: 5px;
   background-color: #fff;
+  width: 100%;
+  max-width: 90%;
+
+  & div.search-header {
+    border-radius: 5px;
+    padding: 10px;
+  }
+
+  & div.search-header h3 {
+    margin: 0;
+  }
+
+  & div.search-header svg {
+    cursor: pointer;
+  }
+
+  & div.search-input {
+    padding: 0 10px;
+    margin: 30px 0;
+  }
+
+  & div.search-input input {
+    border: 1px solid #c1c1c1;
+    box-sizing: border-box;
+    border-radius: 5px;
+    height: 35px;
+    width: 100%;
+  }
+
+  & div.search-content {
+    padding: 0 10px;
+  }
 `;
--- src/components/searchContent/searchContent.tsx
+++ src/components/searchContent/searchContent.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { ModalContainer } from '../modalContainer/styles';
 import { SearchContainer } from './searchContent.styles';
+import { BsFullscreenExit } from 'react-icons/bs';
 
 export default () => (
   <ModalContainer full>
     <SearchContainer>
-      <div>
+      <div className="search-header flex justify-between items-center">
         <h3>Buscar </h3>
-        <span>equis</span>
+        <BsFullscreenExit />
       </div>
-      <div>
+      <div className="search-input">
         <input type="text" />
       </div>
-      <div>
+      <div className="search-content flex flex-col items-center">
         <span>lista</span>
         <span>lista</span>
         <span>lista</span>
