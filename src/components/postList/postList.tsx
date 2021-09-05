c13d31c89424fc936a66f533e40b656848df82ea
Jesus Bossa <x@jesusbossa.dev>
2021-09-05 10:40:46 -0500

    chore: improve re render postlist

--- src/components/postList/postList.tsx
+++ src/components/postList/postList.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { PostCard } from '../';
 import { PostListContainer } from './styles';
 
-export default ({ data }) => (
+const PostList = ({ data }) => (
   <PostListContainer id="main">
     {data.map((el, idx) => (
       <PostCard data={el} key={idx} />
     ))}
   </PostListContainer>
 );
+
+export default React.memo(PostList);
