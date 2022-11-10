88aeabb03e622a049291b91b83ed2512d4c2141c
Jesus Bossa <x@jesusbossa.dev>
2022-11-10 18:04:16 -0500

    chore: change download repo

--- downloadBackup.sh
+++ downloadBackup.sh
@@ -1,4 +1,4 @@
-curl https://download1323.mediafire.com/j5gammyg3gfg/vmos4thf8fu7f2s/archiveImagesBackup.tar.gz -O
-curl https://download1498.mediafire.com/m324cpwfnltg/vkoedzv6le2264f/archiveDocsBackup.tar.gz -O
+curl https://download1501.mediafire.com/2n9c6ceud2bg/h76ght9zha2tv2n/archiveImagesBackup.tar.gz -O
+curl https://download1076.mediafire.com/362vkk82eoug/yhc0ouxqvdilcfy/archiveDocsBackup.tar.gz -O
 tar -zxvf archiveImagesBackup.tar.gz
 tar -zxvf archiveDocsBackup.tar.gz
